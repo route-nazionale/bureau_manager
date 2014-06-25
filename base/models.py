0694312d49ed7aa23e63d30964607ec9452c31e0
tapion <tapion@tapion.it>
2014-06-25 20:53:19 +0200

    Added details to base model

--- base/models.py
+++ base/models.py
@@ -6,7 +6,28 @@ import datetime
 
 class ScoutUnit(models.Model):
 
-    name = models.CharField(max_length=128, primary_key=True)
+    name = models.CharField(
+        max_length=128, 
+        primary_key=True, 
+        verbose_name=u'nome', 
+        help_text=u'Il nome del gruppo scout'
+    )
+    
+    city = models.CharField(
+        max_length=128, 
+        verbose_name=u'città', 
+        help_text=u'La città del gruppo scout'
+    )
+
+    date_arrive = models.DateField(
+        default=None, 
+        verbose_name="data di arrivo", 
+        help_text=u'Data di arrivo'
+    )
+
+    district = models.ForeignKey(District, default=None,
+        verbose_name=u'quartiere', help_text=u'quartiere'
+    )  
 
     class Meta:
 
@@ -19,18 +40,22 @@ class ScoutUnit(models.Model):
 
 #--------------------------------------------------------------------------------
 
-class ScoutChief(models.Model):
+class Scout(models.Model):
 
     code = models.CharField(max_length=128, unique=True,
-        verbose_name="codice censimento"
+        verbose_name="codice censimento", help_text=u'codice censimento'
     )
     scout_unit = models.ForeignKey(Unit)
 
-    name = models.CharField(max_length=32, verbose_name="nome")
-    surname = models.CharField(max_length=32, verbose_name="cognome")
-    birthday = models.DateField(verbose_name="data di nascita");
-    is_spalla = models.BooleanField(default=False, verbose_name=u"è un capo spalla",
-        help_text=u"questo capo verrà iscritto dalla 'pattuglia eventi' con criteri supersonici"
+    name = models.CharField(max_length=32, 
+        verbose_name="nome", help_text=u'nome'
+    )
+    surname = models.CharField(max_length=32, 
+        verbose_name="cognome", help_text=u'cognome'
+    )
+    birthday = models.DateField(verbose_name="data di nascita", help_text=u'data di nascita')
+    is_spalla = models.BooleanField(default=False, 
+        verbose_name=u"è un capo", help_text=u"questo è un capo scout"
     )
 
     class Meta:
@@ -53,8 +78,12 @@ class District(models.Model):
     District entity
     """
 
-    code = models.CharField(max_length=8, primary_key=True)
-    name = models.CharField(max_length=128, unique=True)
+    code = models.CharField(max_length=8, primary_key=True, 
+        verbose_name=u'codice quartiere', help_text=u'codice quartiere'
+    )
+    name = models.CharField(max_length=128, unique=True, 
+        verbose_name=u'nome quartiere', help_text=u'nome quartiere'
+    )
 
     class Meta:
 
@@ -69,8 +98,12 @@ class District(models.Model):
 
 class HeartBeat(models.Model):
 
-    name = models.CharField(max_length=128, unique=True)
-    code = models.IntegerField(blank=True)
+    name = models.CharField(max_length=128, unique=True,
+        verbose_name=u'nome', help_text=u'nome strada di coraggio'
+    )
+    code = models.IntegerField(blank=True,
+        verbose_name=u'codice', help_text=u'codice della strada di coraggio'
+    )
 
     def __unicode__(self):
         return self.name
