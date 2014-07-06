e092865e0e936526eed319f7015a04efb030d756
tapion <tapion@tapion.it>
2014-07-06 12:42:00 +0200

    Rifinito il modello Humen, e migliorata interfaccia di admin

--- edda/admin.py
+++ edda/admin.py
@@ -3,11 +3,74 @@ from edda.models import Humen
 
 class HumenAdmin(admin.ModelAdmin):
     list_display = [
+        '__unicode__',
         'cu',
         'nome',
         'cognome',
+        'vclan_id',
+        'ruolo_id',
         'rs',
         'capo',
     ]
+
+    search_fields = [
+        'cu',
+        'nome',
+        'cognome',
+        'vclan_id__nome',
+        'ruolo_id__description',
+    ]
+
+    list_filter = [
+        'rs',
+        'capo',
+        'ruolo_id__description',
+        'vclan_id__nome',
+    ]
+
+    fieldsets = (
+        (None, {
+            'fields': (
+                'cu', 'codice_censimento', 'idgruppo', 'idunitagruppo', 'vclan_id' 
+            )
+        }),
+        ('Anagrafica', {
+            'fields': (
+                'nome', 'cognome', 'sesso', 'data_nascita', 'eta', 'cellulare', 
+                'email', 'abitazione', 'indirizzo', 'provincia', 'cap', 'citta',
+            )
+        }),
+        ('Partecipazione', {
+            'fields': (
+                'ruolo_id', 'periodo_partecipazione_id', 'pagato', 'mod_pagamento_id', 
+            )
+        }),
+        ('Ruoli', {
+            'fields': (
+                'novizio', 'scout', 'agesci', 'rs', 'capo', 'oneteam', 'extra',  
+            )
+        }),
+        ('Strade di coraggio', {
+            'fields': (
+                'stradadicoraggio1', 'stradadicoraggio2', 'stradadicoraggio3', 'stradadicoraggio4', 'stradadicoraggio5',
+            )
+        }),
+        ('Alimentazione', {
+            'fields': (
+                'colazione', 'dieta_alimentare_id', 'intolleranze_alimentari', 'el_intolleranze_alimentari',
+                'allergie_alimentari', 'el_allergie_alimentari', 'allergie_farmaci', 'el_allergie_farmaci',
+            )
+        }),
+        ('Diversamente abili', {
+            'fields': (
+                'fisiche', 'lis', 'psichiche', 'sensoriali', 'patologie', 
+            )
+        }),
+        ('Aggiornamento', {
+            'fields': (
+                'created_at', 'updated_at', 
+            )
+        }),
+    )
 
 admin.site.register(Humen, HumenAdmin)
--- edda/models.py
+++ edda/models.py
@@ -1,3 +1,5 @@
+#-*- coding: utf-8 -*-
+
 # This is an auto-generated Django model module.
 # You'll have to do the following manually to clean this up:
 #   * Rearrange models' order
@@ -11,6 +13,210 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+class Humen(models.Model):
+    #id = models.IntegerField(primary_key=True)  # AutoField?
+
+    cu = models.CharField(max_length=255, blank=True,
+        verbose_name='codice unico', help_text=''
+    )
+    codice_censimento = models.IntegerField(blank=True, null=True,
+        verbose_name='codice censimento', help_text=''
+    )
+    idgruppo = models.CharField(max_length=255, blank=True,
+        verbose_name='id gruppo', help_text=''
+    )
+    idunitagruppo = models.CharField(max_length=255, blank=True,
+        verbose_name=u'id unità gruppo', help_text=''
+    )
+    
+    vclan_id = models.ForeignKey('Vclans', db_column='vclan_id',
+        verbose_name='clan', help_text=''
+    )
+    
+    # Anagrafiche --------------------------------------
+
+    nome = models.CharField(max_length=255, blank=True,
+        verbose_name='nome', help_text=''
+    )
+    cognome = models.CharField(max_length=255, blank=True,
+        verbose_name='cognome', help_text=''
+    )
+    sesso = models.CharField(max_length=255, blank=True,
+        verbose_name='sesso', help_text=''
+    )
+    data_nascita = models.DateField(blank=True, null=True,
+        verbose_name='data di nascita', help_text=''
+    )
+    eta = models.IntegerField(blank=True, null=True,
+        verbose_name=u'età', help_text=''
+    )
+    cellulare = models.CharField(max_length=255, blank=True,
+        verbose_name='cellulare', help_text=''
+    )
+    email = models.EmailField(max_length=255, blank=True,
+        verbose_name='email', help_text=''
+    )
+    abitazione = models.CharField(max_length=255, blank=True,
+        verbose_name='abitazione', help_text=''
+    )
+    indirizzo = models.CharField(max_length=255, blank=True,
+        verbose_name='indirizzo', help_text=''
+    )
+    provincia = models.CharField(max_length=255, blank=True,
+        verbose_name='provincia', help_text=''
+    )
+    cap = models.CharField(max_length=255, blank=True,
+        verbose_name='CAP', help_text=''
+    )
+    citta = models.CharField(max_length=255, blank=True,
+        verbose_name='citta', help_text=''
+    )
+   
+    # Partecipazione -------------------------------
+ 
+    ruolo_id = models.ForeignKey('Chiefroles', db_column='ruolo_id',
+        verbose_name='ruolo', help_text=''
+    )
+    periodo_partecipazione_id = models.ForeignKey('Periodipartecipaziones', db_column='periodo_partecipazione_id',
+        verbose_name='periodo di partecipazione', help_text=''
+    )
+    pagato = models.BooleanField(default=False,
+        verbose_name='pagato', help_text=''
+    )
+    mod_pagamento_id = models.IntegerField(blank=True, null=True,
+        verbose_name='modalità di pagamento', help_text=''
+    )
+    
+    # Ruoli  ----------------------------
+
+    novizio = models.BooleanField(default=False,
+        verbose_name='novizio', help_text=''
+    )
+    scout = models.BooleanField(default=True,
+        verbose_name='scout', help_text=''
+    )
+    agesci = models.BooleanField(default=True,
+        verbose_name='AGESCI', help_text=''
+    )
+    rs = models.BooleanField(default=True,
+        verbose_name='R/S', help_text=''
+    )
+    capo = models.BooleanField(default=True,
+        verbose_name='capo', help_text=''
+    )
+    oneteam = models.BooleanField(default=False,
+        verbose_name='membro OneTeam', help_text=''
+    )
+    extra = models.BooleanField(default=False,
+        verbose_name='esterno', help_text=''
+    )
+
+    # Strade di coraggio -----------------------------------    
+
+    stradadicoraggio1 = models.IntegerField(blank=True, null=True,
+        verbose_name='strada di coraggio 1', help_text=''
+    )
+    stradadicoraggio2 = models.IntegerField(blank=True, null=True,
+        verbose_name='strada di coraggio 2', help_text=''
+    )
+    stradadicoraggio3 = models.IntegerField(blank=True, null=True,
+        verbose_name='strada di coraggio 3', help_text=''
+    )
+    stradadicoraggio4 = models.IntegerField(blank=True, null=True,
+        verbose_name='strada di coraggio 4', help_text=''
+    )
+    stradadicoraggio5 = models.IntegerField(blank=True, null=True,
+        verbose_name='strada di coraggio 5', help_text=''
+    )
+    
+    # Alimentazione --------------------------------------------
+
+    colazione = models.ForeignKey('Colaziones', db_column='colazione',
+        verbose_name='tipo di colazione', help_text=''
+    )
+    dieta_alimentare_id = models.ForeignKey('Dietabases', db_column='dieta_alimentare_id',
+        verbose_name='dieta alimentare', help_text=''
+    )
+    intolleranze_alimentari = models.IntegerField(blank=True, null=True,
+        verbose_name='intolleranze alimentari', help_text=''
+    )
+    el_intolleranze_alimentari = models.CharField(max_length=255, blank=True,
+        verbose_name='intolleranze alimentari', help_text=''
+    )
+    allergie_alimentari = models.IntegerField(blank=True, null=True,
+        verbose_name='allergie alimentari', help_text=''
+    )
+    el_allergie_alimentari = models.CharField(max_length=255, blank=True,
+        verbose_name='allergie alimentari', help_text=''
+    )
+    allergie_farmaci = models.IntegerField(blank=True, null=True,
+        verbose_name='allergie farmaci', help_text=''
+    )
+    el_allergie_farmaci = models.CharField(max_length=255, blank=True,
+        verbose_name='allergie farmaci', help_text=''
+    )
+
+    # Diversamente abili ---------------------------------------
+
+    fisiche = models.IntegerField(blank=True, null=True,
+        verbose_name=u'disabilità fisica', help_text=''
+    )
+    lis = models.IntegerField(blank=True, null=True,
+        verbose_name='LIS', help_text=''
+    )
+    psichiche = models.IntegerField(blank=True, null=True,
+        verbose_name=u'disabilità psichica', help_text=''
+    )
+    sensoriali = models.IntegerField(blank=True, null=True,
+        verbose_name=u'disabilità sensoriale', help_text=''
+    )
+    patologie = models.CharField(max_length=255, blank=True,
+        verbose_name=u'patologie', help_text=''
+    )
+
+
+    # Date di creazione ed aggiornamento
+
+    created_at = models.DateTimeField(blank=True, null=True,
+        verbose_name='data di creazione', help_text=''
+    )
+    updated_at = models.DateTimeField(blank=True, null=True,
+        verbose_name='ultimo aggiornamento', help_text=''
+    )
+
+    # ???
+    lab = models.IntegerField(blank=True, null=True,
+        verbose_name='lab??', help_text=''
+    )
+
+    class Meta:
+        managed = False
+        db_table = 'humen'
+        verbose_name = 'persona'
+        verbose_name_plural = 'persone'
+
+    def __unicode__(self):
+        return "%s - %s %s" % (self.cu, self.nome, self.cognome)
+
+class Vclans(models.Model):
+    id = models.IntegerField(primary_key=True)  # AutoField?
+    idvclan = models.CharField(max_length=255, blank=True)
+    idgruppo = models.CharField(max_length=255, blank=True)
+    idunitagruppo = models.CharField(max_length=255, blank=True)
+    ordinale = models.CharField(max_length=255, blank=True)
+    nome = models.CharField(max_length=255, blank=True)
+    regione = models.CharField(max_length=255, blank=True)
+    updated_at = models.DateTimeField(blank=True, null=True)
+    created_at = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'vclans'
+        verbose_name = 'clan'
+        verbose_name_plural = 'clan'
+
+    def __unicode__(self):
+        return self.nome
 
 class Chiefroles(models.Model):
     id = models.IntegerField(primary_key=True)  # AutoField?
@@ -22,7 +228,11 @@ class Chiefroles(models.Model):
     class Meta:
         managed = False
         db_table = 'chiefroles'
+        verbose_name = 'ruolo'
+        verbose_name_plural = 'ruoli'
 
+    def __unicode__(self):
+        return self.description
 
 class Colaziones(models.Model):
     id = models.IntegerField(primary_key=True)  # AutoField?
@@ -34,7 +244,11 @@ class Colaziones(models.Model):
     class Meta:
         managed = False
         db_table = 'colaziones'
+        verbose_name = 'tipo di colazione'
+        verbose_name_plural = 'tipi di colazione'
 
+    def __unicode__(self):
+        return self.name
 
 class Contradas(models.Model):
     id = models.IntegerField(primary_key=True)  # AutoField?
@@ -50,7 +264,11 @@ class Contradas(models.Model):
     class Meta:
         managed = False
         db_table = 'contradas'
+        verbose_name = 'contrada'
+        verbose_name_plural = 'contrade'
 
+    def __unicode__(self):
+        return self.name
 
 class Dietabases(models.Model):
     id = models.IntegerField(primary_key=True)  # AutoField?
@@ -62,7 +280,11 @@ class Dietabases(models.Model):
     class Meta:
         managed = False
         db_table = 'dietabases'
+        verbose_name = 'dieta alimentare'
+        verbose_name_plural = 'diete alimentari'
 
+    def __unicode__(self):
+        return self.name
 
 class Districts(models.Model):
     id = models.IntegerField(primary_key=True)  # AutoField?
@@ -77,18 +299,11 @@ class Districts(models.Model):
     class Meta:
         managed = False
         db_table = 'districts'
+        verbose_name = 'quartiere'
+        verbose_name_plural = 'quartieri'
 
-
-class DjangoMigrations(models.Model):
-    id = models.IntegerField(primary_key=True)  # AutoField?
-    app = models.CharField(max_length=255)
-    name = models.CharField(max_length=255)
-    applied = models.DateTimeField()
-
-    class Meta:
-        managed = False
-        db_table = 'django_migrations'
-
+    def __unicode__(self):
+        return self.name
 
 class Gemellaggios(models.Model):
     id = models.IntegerField(primary_key=True)  # AutoField?
@@ -101,64 +316,8 @@ class Gemellaggios(models.Model):
     class Meta:
         managed = False
         db_table = 'gemellaggios'
-
-
-class Humen(models.Model):
-    id = models.IntegerField(primary_key=True)  # AutoField?
-    cu = models.CharField(max_length=255, blank=True)
-    codice_censimento = models.IntegerField(blank=True, null=True)
-    idgruppo = models.CharField(max_length=255, blank=True)
-    idunitagruppo = models.CharField(max_length=255, blank=True)
-    vclan_id = models.IntegerField(blank=True, null=True)
-    nome = models.CharField(max_length=255, blank=True)
-    cognome = models.CharField(max_length=255, blank=True)
-    sesso = models.CharField(max_length=255, blank=True)
-    data_nascita = models.DateField(blank=True, null=True)
-    eta = models.IntegerField(blank=True, null=True)
-    periodo_partecipazione_id = models.IntegerField(blank=True, null=True)
-    ruolo_id = models.IntegerField(blank=True, null=True)
-    novizio = models.IntegerField(blank=True, null=True)
-    scout = models.IntegerField(blank=True, null=True)
-    agesci = models.IntegerField(blank=True, null=True)
-    rs = models.IntegerField(blank=True, null=True)
-    capo = models.IntegerField(blank=True, null=True)
-    email = models.CharField(max_length=255, blank=True)
-    abitazione = models.CharField(max_length=255, blank=True)
-    indirizzo = models.CharField(max_length=255, blank=True)
-    provincia = models.CharField(max_length=255, blank=True)
-    cap = models.CharField(max_length=255, blank=True)
-    citta = models.CharField(max_length=255, blank=True)
-    pagato = models.IntegerField(blank=True, null=True)
-    mod_pagamento_id = models.IntegerField(blank=True, null=True)
-    colazione = models.IntegerField(blank=True, null=True)
-    dieta_alimentare_id = models.IntegerField(blank=True, null=True)
-    intolleranze_alimentari = models.IntegerField(blank=True, null=True)
-    el_intolleranze_alimentari = models.CharField(max_length=255, blank=True)
-    allergie_alimentari = models.IntegerField(blank=True, null=True)
-    el_allergie_alimentari = models.CharField(max_length=255, blank=True)
-    allergie_farmaci = models.IntegerField(blank=True, null=True)
-    el_allergie_farmaci = models.CharField(max_length=255, blank=True)
-    fisiche = models.IntegerField(blank=True, null=True)
-    lis = models.IntegerField(blank=True, null=True)
-    psichiche = models.IntegerField(blank=True, null=True)
-    sensoriali = models.IntegerField(blank=True, null=True)
-    patologie = models.CharField(max_length=255, blank=True)
-    stradadicoraggio1 = models.IntegerField(blank=True, null=True)
-    stradadicoraggio2 = models.IntegerField(blank=True, null=True)
-    stradadicoraggio3 = models.IntegerField(blank=True, null=True)
-    stradadicoraggio4 = models.IntegerField(blank=True, null=True)
-    stradadicoraggio5 = models.IntegerField(blank=True, null=True)
-    created_at = models.DateTimeField(blank=True, null=True)
-    updated_at = models.DateTimeField(blank=True, null=True)
-    cellulare = models.CharField(max_length=255, blank=True)
-    oneteam = models.IntegerField(blank=True, null=True)
-    extra = models.IntegerField(blank=True, null=True)
-    lab = models.IntegerField(blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'humen'
-
+        verbose_name = 'gemellaggio'
+        verbose_name_plural = 'gemellaggi'
 
 class Periodipartecipaziones(models.Model):
     id = models.IntegerField(primary_key=True)  # AutoField?
@@ -175,7 +334,11 @@ class Periodipartecipaziones(models.Model):
     class Meta:
         managed = False
         db_table = 'periodipartecipaziones'
-
+        verbose_name = 'periodo di partecipazione'
+        verbose_name_plural = 'periodi di partecipazione'
+    
+    def __unicode__(self):
+        return self.description
 
 class RoutesOrig(models.Model):
     id = models.IntegerField(primary_key=True)  # AutoField?
@@ -192,7 +355,6 @@ class RoutesOrig(models.Model):
         managed = False
         db_table = 'routes_orig'
 
-
 class RoutesTest(models.Model):
     id = models.IntegerField(primary_key=True)  # AutoField?
     name = models.CharField(max_length=255, blank=True)
@@ -208,15 +370,6 @@ class RoutesTest(models.Model):
         managed = False
         db_table = 'routes_test'
 
-
-class SchemaMigrations(models.Model):
-    version = models.CharField(unique=True, max_length=255)
-
-    class Meta:
-        managed = False
-        db_table = 'schema_migrations'
-
-
 class Topics(models.Model):
     id = models.IntegerField(primary_key=True)  # AutoField?
     kkey = models.IntegerField(blank=True, null=True)
@@ -228,18 +381,23 @@ class Topics(models.Model):
         managed = False
         db_table = 'topics'
 
+# ----------------------------------
 
-class Vclans(models.Model):
-    id = models.IntegerField(primary_key=True)  # AutoField?
-    idvclan = models.CharField(max_length=255, blank=True)
-    idgruppo = models.CharField(max_length=255, blank=True)
-    idunitagruppo = models.CharField(max_length=255, blank=True)
-    ordinale = models.CharField(max_length=255, blank=True)
-    nome = models.CharField(max_length=255, blank=True)
-    regione = models.CharField(max_length=255, blank=True)
-    updated_at = models.DateTimeField(blank=True, null=True)
-    created_at = models.DateTimeField(blank=True, null=True)
+class SchemaMigrations(models.Model):
+    version = models.CharField(unique=True, max_length=255)
 
     class Meta:
         managed = False
-        db_table = 'vclans'
+        db_table = 'schema_migrations'
+
+
+
+class DjangoMigrations(models.Model):
+    id = models.IntegerField(primary_key=True)  # AutoField?
+    app = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    applied = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_migrations'
