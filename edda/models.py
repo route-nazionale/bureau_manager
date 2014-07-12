5d0267176b03f4d57f55065915239c4defa2d3e6
Luca Ferroni <luca@befair.it>
2014-07-12 17:30:06 +0200

    Gestione arrivo al campo e al quartiere nei modelli
    okkio che nel prossimo commit ci vanno le migrazioni del db

--- edda/models.py
+++ edda/models.py
@@ -15,6 +15,7 @@ from django.db import models
 from django.conf import settings
 
 import logging
+from django.utils.timezone import now
 
 logger = logging.getLogger('__name__')
 
@@ -192,6 +193,9 @@ class Humen(models.Model):
         verbose_name='ultimo aggiornamento', help_text='', auto_now=True
     )
 
+    arrivati_al_quartiere = models.NullBooleanField(default=None)
+    dt_verifica_di_arrivo = models.DateTimeField(blank=True, null=True)
+
     class Meta:
         managed = False
         db_table = 'humen'
@@ -260,6 +264,12 @@ class Humen(models.Model):
 
         return compute_age(self.data_nascita) < settings.YOUNG_AGE
 
+    def update_arrivo_al_quartiere(self, is_arrived):
+        if is_arrived and self.vclan and not self.vclan.is_arrived:
+            raise ValueError("Una persona non può essere arrivata se non è arrivato il suo clan")
+        self.arrivati_al_quartiere = is_arrived
+        self.dt_verifica_di_arrivo = now()
+
 class Vclans(models.Model):
 
     idvclan = models.CharField(max_length=255, blank=True)
@@ -268,9 +278,12 @@ class Vclans(models.Model):
     ordinale = models.CharField(max_length=255, blank=True)
     nome = models.CharField(max_length=255, blank=True)
     regione = models.CharField(max_length=255, blank=True)
-    updated_at = models.DateTimeField(blank=True, null=True)
-    created_at = models.DateTimeField(blank=True, null=True)
+    updated_at = models.DateTimeField(blank=True, null=True, auto_now=True)
+    created_at = models.DateTimeField(blank=True, null=True, auto_now_add=True)
 
+    arrivati_al_campo = models.NullBooleanField(default=None)
+    dt_verifica_di_arrivo = models.DateTimeField(blank=True, null=True)
+    
     class Meta:
         managed = False
         db_table = 'vclans'
@@ -279,6 +292,10 @@ class Vclans(models.Model):
 
     def __unicode__(self):
         return u"%s (%s)" % (self.nome, self.idunitagruppo)
+
+    def update_arrivo_al_campo(self, is_arrived):
+        self.arrivati_al_campo = is_arrived
+        self.dt_verifica_di_arrivo = now()
 
 class Chiefroles(models.Model):
     kkey = models.IntegerField(blank=True, null=True)
