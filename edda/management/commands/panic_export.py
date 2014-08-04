aace8136ef6e700a68fc64aafe3ee3511f78aa75
tapion <tapion@tapion.it>
2014-08-05 00:59:27 +0200

    Panic button (da finire)

---
+++ edda/management/commands/panic_export.py
@@ -0,0 +1,69 @@
+#-*- coding: utf-8 -*-
+
+from django.core.management.base import BaseCommand, CommandError
+
+from django.template.loader import get_template
+from django.template import Context
+
+from edda.models import *
+
+from xhtml2pdf import pisa
+import cStringIO as StringIO
+import cgi
+import sys
+import datetime
+
+class Command(BaseCommand):
+    args = ''
+    help = 'print info PDF for all events, listing people'
+
+    def handle(self, *args, **options):
+
+        #hs = Humen.objects.filter(arrivato_al_quartiere=True)
+        hs = Humen.objects.all()
+        n_hs = hs.count()
+        done = 0
+
+        con = {}
+        con['hs'] = []
+
+        for h in hs:
+
+            done = done + 1
+
+            sys.stdout.write("\r%s/%s %s %s%%" % (done, n_hs, h.cu, int(100 * float(done)/float(n_hs))))
+            sys.stdout.flush()
+
+            con['hs'].append(h)
+        
+        a = datetime.datetime.now()
+
+        con['data'] = a
+        con['n_hs'] = n_hs
+        
+
+        filename = '/tmp/panic_export_humen_%s%s%s-%s%s.pdf' % (
+            a.year,
+            a.month,
+            a.day,
+            a.hour,
+            a.minute,
+        )
+
+        print ''
+        print 'esportati %s utenti in %s' % (done, filename) 
+
+        write_pdf('panic_export.html', con, filename)
+
+
+def write_pdf(template_src, context_dict, filename):
+    template = get_template(template_src)
+    context = Context(context_dict)
+    html  = template.render(context)
+    result = open(filename, 'wb') # Changed from file to filename
+    pdf = pisa.pisaDocument(StringIO.StringIO(
+        html.encode("UTF-8")), result)
+    result.close()
+
+
+          
